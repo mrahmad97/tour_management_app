b210f07495f45d6a30cfbeee46598e14059248a9
mrahmad97 <mrahmad9197@gmail.com>
2024-12-03 22:44:30 +0500

    firebase integration

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -12,7 +12,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("HomePage"),
+        title: Text("Home"),
       ),
       body: Center(
         child: Text('Homepage'),
